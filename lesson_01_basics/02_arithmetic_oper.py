8aee0b7cc0ba4ec8cd8f76f3ae9d6078740ea899
kellytre <kellytreskunov@gmail.com>
2023-07-19 18:14:14 +0300

    first1 commit

---
+++ lesson_01_basics/02_arithmetic_oper.py
@@ -0,0 +1,48 @@
+# number_1 = 5
+# number_2 = 3
+
+#print(number_1 + number_2)
+# print(f"{number_1} + {number_2} = {(number_1 + number_2)}")
+
+# print(f"{number_1} - {number_2} = {(number_1 - number_2)}")
+
+# print(f"{number_1} * {number_2} = {(number_1 * number_2)}")
+
+# print(type(number_1 * number_2))
+
+# number_3 = 2.0
+
+# print(f"{number_1} * {number_3} = {(number_1 * number_3)}")
+# print(type(number_1 * number_3))
+
+# print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+ # number_1 = 2
+ # number_2 = 3
+
+# print(f"{number_1} ** {number_2} = {(number_1 ** number_2)}")
+
+# text_1 = "kelly"
+# text_2 = "treskunov"
+
+# print(f"{text_1} + {text_2} = {(text_1 + text_2)}")
+
+# text_1 = "**"
+# print(f"{text_1} * {number_2} = {(text_1 * number_2)}")
+
+number_1 = 19
+number_2 = 5
+
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")
+print(f"{number_1} % {number_2} = {(number_1 % number_2)}")
+
+
+
+
+
+
+
+
+
+
+
