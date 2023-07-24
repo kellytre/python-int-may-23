dcd8786a4b9977386468e5cb468276296a3dd3f2
kellytre <kellytreskunov@gmail.com>
2023-07-24 18:36:41 +0300

    update

--- lesson_01_basics/first_app.py
+++ lesson_01_basics/first_app.py
@@ -1,3 +1,42 @@
-print('kelly')
+a = 10
+print(a)
+a = "kelly"
+print(a)
+
+name = "kelly"
+_last_nam = "treskunov"
 age = 26
-print(age)
+Age = 27
+# a and A not the same.
+# variables can't start with number.
+# b1 b2 b3 - not recommended to usage.
+
+my_list = [1,"kelly",2,"blala"]
+print(my_list)
+print(type(my_list))
+
+number_1 = 10
+number_2 = 1000
+
+print(id(number_1))
+print(id(number_2))
+
+# del number_1 - delete the variable frome the memory
+#print id - print id for the value
+
+int_1 = 56
+int_2 = 67584756
+int_3 = -345
+
+float_1 = 1.45
+float_2 = -1.67
+float_3 = 35e3
+
+print(float_3)
+
+complex_1 = 5J
+print(complex_1)
+
+
+
+
